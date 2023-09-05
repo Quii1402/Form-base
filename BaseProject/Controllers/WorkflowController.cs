17d9aac00bf81caad73a07e393f59fea7b064dd9
qui.nb@dtvn.kyocera.com <qui.nb@dtvn.kyocera.com>
2023-09-05 13:16:09 +0700

    first commit

---
+++ BaseProject/Controllers/WorkflowController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaseProject.Controllers
+{
+    public class WorkflowController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+    }
+}
